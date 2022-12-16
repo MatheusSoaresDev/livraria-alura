947ff72b88ad13ebd819efb3bcd1e49ccb64ac45
Matheus Soares <matheus.soares@rarolabs.com.br>
2022-12-15 23:37:00 -0300

    Rotas criadas no arquivo routers e criação dos metodo do crud de livros na controller livros.

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -3,6 +3,11 @@ import LivroController from "../controllers/livrosController.js";
 
 const router = express.Router();
 
-router.get("/livros", LivroController.listarLivros);
+router
+    .post("/livros", LivroController.cadastrarLivro)
+    .get("/livros", LivroController.listarLivros)
+    .get("/livros/:id", LivroController.listarLivroPorId)
+    .put("/livros/:id", LivroController.atualizarLivro)
+    .delete("/livros/:id", LivroController.excluirLivro)
 
 export default router;
